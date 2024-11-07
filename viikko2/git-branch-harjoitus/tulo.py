f4c40f5b3d11533f15d8a0b17c5f187b3acb8750
simo <49877847+saimouu@users.noreply.github.com>
2024-11-07 11:17:44 +0200

    fix

--- viikko2/git-branch-harjoitus/tulo.py
+++ viikko2/git-branch-harjoitus/tulo.py
@@ -1,15 +1,2 @@
-from logger import logger
-from summa import summa
-from erotus import erotus
-from tulo import tulo
-
-logger("aloitetaan ohjelma")
-
-x = int(input("luku 1: "))
-y = int(input("luku 2: "))
-print(f"{x} + {y} = {summa(x, y)}") 
-print(f"{x} - {y} = {erotus(x, y)}") 
-print(f"{x} * {y} = {tulo(x, y)}") 
-
-logger("lopetetaan")
-print("goodbye!")
+def tulo(x, y):
+    return x*y
