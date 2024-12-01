b80953a5413c010cb6cb65d272d5c2dc89a91029
simo <sipilasimo@gmail.com>
2024-12-01 14:58:48 +0200

    teht. 3-4

--- viikko5/int-joukko/src/int_joukko.py
+++ viikko5/int-joukko/src/int_joukko.py
@@ -3,22 +3,17 @@ OLETUSKASVATUS = 5
 
 
 class IntJoukko:
-    # tämä metodi on ainoa tapa luoda listoja
     def _luo_lista(self, koko):
         return [0] * koko
-    
-    def __init__(self, kapasiteetti=None, kasvatuskoko=None):
-        if kapasiteetti is None:
-            self.kapasiteetti = KAPASITEETTI
-        elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("Väärä kapasiteetti")  # heitin vaan jotain :D
+
+    def __init__(self, kapasiteetti=KAPASITEETTI, kasvatuskoko=OLETUSKASVATUS):
+        if not isinstance(kapasiteetti, int) or kapasiteetti < 0:
+            raise Exception("Väärä kapasiteetti")
         else:
             self.kapasiteetti = kapasiteetti
 
-        if kasvatuskoko is None:
-            self.kasvatuskoko = OLETUSKASVATUS
-        elif not isinstance(kapasiteetti, int) or kapasiteetti < 0:
-            raise Exception("kapasiteetti2")  # heitin vaan jotain :D
+        if not isinstance(kasvatuskoko, int) or kasvatuskoko < 0:
+            raise Exception("Väärä kasvatuskoko")
         else:
             self.kasvatuskoko = kasvatuskoko
 
@@ -27,66 +22,42 @@ class IntJoukko:
         self.alkioiden_lkm = 0
 
     def kuuluu(self, n):
-        on = 0
-
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                on = on + 1
-
-        if on > 0:
-            return True
-        else:
-            return False
+        return n in self.ljono
 
     def lisaa(self, n):
-        ei_ole = 0
+        if self.kuuluu(n):
+            return False
 
-        if self.alkioiden_lkm == 0:
-            self.ljono[0] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
-            return True
-        else:
-            pass
+        self.ljono[self.alkioiden_lkm] = n
+        self.alkioiden_lkm += 1
 
-        if not self.kuuluu(n):
-            self.ljono[self.alkioiden_lkm] = n
-            self.alkioiden_lkm = self.alkioiden_lkm + 1
+        if self.alkioiden_lkm % len(self.ljono) == 0:
+            self._kavata_listaa()
 
-            # ei mahdu enempää, luodaan uusi säilytyspaikka luvuille
-            if self.alkioiden_lkm % len(self.ljono) == 0:
-                taulukko_old = self.ljono
-                self.kopioi_lista(self.ljono, taulukko_old)
-                self.ljono = self._luo_lista(self.alkioiden_lkm + self.kasvatuskoko)
-                self.kopioi_lista(taulukko_old, self.ljono)
+        return True
 
-            return True
-
-        return False
+    def _kavata_listaa(self):
+        vanha_taulukko = self.ljono
+        self.ljono = self._luo_lista(self.alkioiden_lkm + self.kasvatuskoko)
+        self.kopioi_lista(vanha_taulukko, self.ljono)
 
     def poista(self, n):
-        kohta = -1
-        apu = 0
+        try:
+            kohta = self.ljono.index(n)
+        except ValueError:
+            return False
 
-        for i in range(0, self.alkioiden_lkm):
-            if n == self.ljono[i]:
-                kohta = i  # siis luku löytyy tuosta kohdasta :D
-                self.ljono[kohta] = 0
-                break
+        for i in range(kohta, self.alkioiden_lkm - 1):
+            self.ljono[i] = self.ljono[i + 1]
 
-        if kohta != -1:
-            for j in range(kohta, self.alkioiden_lkm - 1):
-                apu = self.ljono[j]
-                self.ljono[j] = self.ljono[j + 1]
-                self.ljono[j + 1] = apu
+        self.alkioiden_lkm -= 1
+        self.ljono[self.alkioiden_lkm] = 0
 
-            self.alkioiden_lkm = self.alkioiden_lkm - 1
-            return True
+        return True
 
-        return False
-
-    def kopioi_lista(self, a, b):
-        for i in range(0, len(a)):
-            b[i] = a[i]
+    def kopioi_lista(self, vanha_taulukko, uusi_taulukko):
+        for i in range(0, len(vanha_taulukko)):
+            uusi_taulukko[i] = vanha_taulukko[i]
 
     def mahtavuus(self):
         return self.alkioiden_lkm
@@ -105,24 +76,21 @@ class IntJoukko:
         a_taulu = a.to_int_list()
         b_taulu = b.to_int_list()
 
-        for i in range(0, len(a_taulu)):
-            x.lisaa(a_taulu[i])
-
-        for i in range(0, len(b_taulu)):
-            x.lisaa(b_taulu[i])
+        for i in a_taulu + b_taulu:
+            x.lisaa(i)
 
         return x
 
     @staticmethod
     def leikkaus(a, b):
         y = IntJoukko()
-        a_taulu = a.to_int_list()
-        b_taulu = b.to_int_list()
+        a_taulu = set(a.to_int_list())
+        b_taulu = set(b.to_int_list())
 
-        for i in range(0, len(a_taulu)):
-            for j in range(0, len(b_taulu)):
-                if a_taulu[i] == b_taulu[j]:
-                    y.lisaa(b_taulu[j])
+        yhteiset_luvut = a_taulu & b_taulu
+
+        for i in yhteiset_luvut:
+            y.lisaa(i)
 
         return y
 
@@ -141,15 +109,5 @@ class IntJoukko:
         return z
 
     def __str__(self):
-        if self.alkioiden_lkm == 0:
-            return "{}"
-        elif self.alkioiden_lkm == 1:
-            return "{" + str(self.ljono[0]) + "}"
-        else:
-            tuotos = "{"
-            for i in range(0, self.alkioiden_lkm - 1):
-                tuotos = tuotos + str(self.ljono[i])
-                tuotos = tuotos + ", "
-            tuotos = tuotos + str(self.ljono[self.alkioiden_lkm - 1])
-            tuotos = tuotos + "}"
-            return tuotos
+        luvut_str = ", ".join(str(i) for i in self.ljono[: self.alkioiden_lkm])
+        return f"{{{luvut_str}}}"
--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -5,51 +5,40 @@ class TennisGame:
         self.m_score1 = 0
         self.m_score2 = 0
 
+    def _get_point_names(self):
+        return ["Love", "Fifteen", "Thirty", "Forty"]
+
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.m_score1 += 1
+        elif player_name == self.player2_name:
+            self.m_score2 += 1
+
+    def _check_win(self, score_difference):
+        if score_difference == 1:
+            return f"Advantage {self.player1_name}"
+        elif score_difference == -1:
+            return f"Advantage {self.player2_name}"
+        elif score_difference >= 2:
+            return f"Win for {self.player1_name}"
         else:
-            self.m_score2 = self.m_score2 + 1
+            return f"Win for {self.player2_name}"
+
+    def _equal_score(self):
+        if self.m_score1 <= 2:
+            return f"{self._get_point_names()[self.m_score1]}-All"
+        else:
+            return "Deuce"
+
+    def _generate_score(self):
+        point_names = self._get_point_names()
+        return f"{point_names[self.m_score1]}-{point_names[self.m_score2]}"
 
     def get_score(self):
-        score = ""
-        temp_score = 0
-
         if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
+            return self._equal_score()
         elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            score_difference = self.m_score1 - self.m_score2
+            return self._check_win(score_difference)
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
-
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
-
-        return score
+            return self._generate_score()
